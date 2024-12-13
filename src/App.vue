212ebf322fc772593533699e1fa61ba22263f50c
Alicia Alexander <aalex@Mac.hsd1.va.comcast.net>
2024-12-13 02:35:22 -0500

    Interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: true
+    }
+  }
+}
+</script>
+
 
 <template>
-  <h1>Vue jobs</h1>
-  
+  <h1>{{ name }}</h1>
+  <p v-if="status">User is active</p>
 </template>
 
 
